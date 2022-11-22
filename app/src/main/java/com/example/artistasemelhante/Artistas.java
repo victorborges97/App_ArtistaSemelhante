deb50dcc05b114630d96aaa001b58efbb3d06638
DE-TECH <caiogarcia8@gmail.com>
2022-11-22 15:13:10 -0300

    Atualização dia 22/11/22

---
+++ app/src/main/java/com/example/artistasemelhante/Artistas.java
@@ -0,0 +1,12 @@
+package com.example.artistasemelhante;
+
+
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface Artistas {
+
+}
+
+
--- app/src/main/java/com/example/artistasemelhante/Recomendacoes.kt
+++ app/src/main/java/com/example/artistasemelhante/Recomendacoes.kt
@@ -2,10 +2,25 @@ package com.example.artistasemelhante
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Recomendacoes : AppCompatActivity() {
+
+    lateinit var recomendacao_um : TextView
+    lateinit var recomendacao_dois : TextView
+    lateinit var recomendacao_tres : TextView
+    lateinit var recomendacao_quatro : TextView
+    lateinit var recomendacao_cinco : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recomendacoes)
+
+        recomendacao_um = findViewById(R.id.recomendacao_um)
+        recomendacao_dois = findViewById(R.id.recomendacao_dois)
+        recomendacao_tres = findViewById(R.id.recomendacao_tres)
+        recomendacao_quatro = findViewById(R.id.recomendacao_quatro)
+        recomendacao_cinco = findViewById(R.id.recomendacao_cinco)
+
     }
 }
